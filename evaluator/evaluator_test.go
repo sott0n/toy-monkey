495085b5f26ae23135a2d14b9dc1c109ba5d080e
Kohei Yamaguchi <fix7211@gmail.com>
2018-10-09 01:27:26 +0900

    Add string + string operator into eval.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -48,6 +48,20 @@ func TestStringLiteral(t *testing.T) {
 	}
 }
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -242,6 +256,10 @@ if (10 > 1) {
 		{
 			"foobar",
 			"identifier not found: foobar",
+		},
+		{
+			`"Hello" - "World!"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 
