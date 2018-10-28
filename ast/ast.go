132b254c90b51bf238c503b4c590aca43b21d5f1
Kohei Yamaguchi <fix7211@gmail.com>
2018-10-28 18:35:10 +0900

    Add index parser.

--- ast/ast.go
+++ ast/ast.go
@@ -288,7 +288,27 @@ func (al *ArrayLiteral) String() string {
 
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
-	out.WriteString(")")
+	out.WriteString("]")
+
+	return out.String()
+}
+
+type IndexExpression struct {
+	Token token.Token // '[' token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("])")
 
 	return out.String()
 }
